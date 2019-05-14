ebe72e603a9ce387c36edab53e030185b25c5ff7
SilverBullete <1256819071@qq.com>
2019-05-14 12:47:28 +0800

    bugfix

--- home_application/admin.py
+++ home_application/admin.py
@@ -6,9 +6,13 @@
 # import from lib
 # ===============================================================================
 from django.contrib import admin
-from models import Choice, Permission, Organization
+from models import Choice, Permission, Organization, UserInfo, UserPermission, Award, Form
 
 admin.site.register(Choice)
 admin.site.register(Permission)
 admin.site.register(Organization)
+admin.site.register(UserInfo)
+admin.site.register(UserPermission)
+admin.site.register(Award)
+admin.site.register(Form)
 # ===============================================================================
