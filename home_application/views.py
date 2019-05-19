705972fc7ac6bc2f628847fd6f34286578e36641
hanyuntao <1181849919@qq.com>
2019-05-19 21:46:59 +0800

    feature:首页登录时取uin存到表UserInfo中；在models的Award中添加筛选奖项的功能（使用在个人中心中）

--- home_application/views.py
+++ home_application/views.py
@@ -1,14 +1,20 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 
+from bkoauth.utils import transform_uin
 from common.mymako import render_mako_context
 
 
 # 开发框架中通过中间件默认是需要登录态的，如有不需要登录的，可添加装饰器login_exempt【装饰器引入from account.decorators import login_exempt】
-from home_application.models import Award, Form
+from home_application.models import Award, Form, UserInfo
 
 
 def home(request):
+    qq = request.COOKIES.get('uin', '')
+    qq = transform_uin(qq)
+    user = UserInfo.objects.filter(qq=qq)
+    if not user:
+        UserInfo.objects.create(qq=qq, auth_token=request.user)
     awards_list = Award.objects.filter(status=True).order_by('-id')[:3]
     awards = Award.to_array(awards_list)
     prize_winner = Form.objects.filter(status=4).order_by('-form_id')
